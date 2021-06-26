854d9b082b16ba407fe558c69cc8a557a02d78d9
Priyanshu Raj <rpriyanshu9@gmail.com>
2021-06-26 14:39:05 +0530

    set up API calls using mapbox and openweather APIs

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 require('dotenv').config()
-const axios = require('./axios')
+const axios = require('axios')
 
-const getReq = async () => {
+const getReq = async (coordinates, city) => {
     try {
-        const response = await axios.get(`http://api.weatherstack.com/current?access_key=${process.env.API_KEY}&query=New York`)
-        console.log(response.data)
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${coordinates[1]}&lon=${coordinates[0]}&appid=${process.env.API_KEY}&units=metric`)
+        const data = response.data
+        console.log(`It is currently ${data.main.temp} degrees out, with ${data.main.humidity}% humidity in ${city}.`)
     } catch (e) {
         console.log(e)
     }
 }
 
-getReq()
+const forwardGeocodingReq = async (cityName) => {
+    try {
+        const response = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${cityName}.json?access_token=${process.env.ACCESS_TOKEN}`)
+        return response.data.features[0].center
+    } catch (error) {
+        console.log(error)
+        return []
+    }
+}
+
+forwardGeocodingReq("Aligarh").then((res) => {
+    getReq(res, city)
+})
+
 
--- axios.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios').default
-
-const instance = axios.create({
-    baseURL: 'http://api.weatherstack.com/'
-})
-
-module.exports = instance
